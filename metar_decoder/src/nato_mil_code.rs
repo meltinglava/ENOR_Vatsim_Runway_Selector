336d16b75c2d972d136e533cd73ad8589835c7cc
meltinglava <meltinglavaoutland@gmail.com>
2025-10-12 21:35:48 +0200

    add modifier to nato_mil_codes

--- metar_decoder/src/nato_mil_code.rs
+++ metar_decoder/src/nato_mil_code.rs
@@ -1,7 +1,20 @@
-use nom::{IResult, Parser, branch::alt, bytes::complete::tag, combinator::value};
+use nom::{
+    IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::one_of,
+    combinator::{opt, value},
+    multi::many1,
+};
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct NatoMilCode {
+    pub codes: Vec<NatoMilCodeType>,
+    pub modifier: Option<char>,
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum NatoMilCode {
+pub enum NatoMilCodeType {
     Blue,
     White,
     Green,
@@ -12,14 +25,18 @@ pub enum NatoMilCode {
 }
 
 pub(crate) fn nom_nato_mil_code(input: &str) -> IResult<&str, NatoMilCode> {
-    alt((
-        value(NatoMilCode::Blue, tag("BLU")),
-        value(NatoMilCode::White, tag("WHT")),
-        value(NatoMilCode::Green, tag("GRN")),
-        value(NatoMilCode::Yellow, tag("YLO")),
-        value(NatoMilCode::Amber, tag("AMB")),
-        value(NatoMilCode::Red, tag("RED")),
-        value(NatoMilCode::Black, tag("BLACK")),
-    ))
-    .parse(input)
+    (
+        many1(alt((
+            value(NatoMilCodeType::Blue, tag("BLU")),
+            value(NatoMilCodeType::White, tag("WHT")),
+            value(NatoMilCodeType::Green, tag("GRN")),
+            value(NatoMilCodeType::Yellow, tag("YLO")),
+            value(NatoMilCodeType::Amber, tag("AMB")),
+            value(NatoMilCodeType::Red, tag("RED")),
+            value(NatoMilCodeType::Black, tag("BLACK")),
+        ))),
+        opt(one_of("+-")),
+    )
+        .map(|(codes, modifier)| NatoMilCode { codes, modifier })
+        .parse(input)
 }
