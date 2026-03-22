6a8fefd2c71276b56ab93d65fc1e300c487e5eb3
meltinglava <meltinglavaoutland@gmail.com>
2026-03-22 13:40:58 +0100

    fix backtracking for metars that are old

--- metar_decoder/src/units/timestamp.rs
+++ metar_decoder/src/units/timestamp.rs
@@ -1,11 +1,16 @@
 use std::fmt::Display;
 
-use jiff::{Zoned, civil::date, tz::TimeZone};
+use jiff::{
+    Zoned,
+    civil::{Date, DateTime, Time},
+    tz::TimeZone,
+};
 use nom::{
     IResult, Parser,
     bytes::complete::take,
     character::complete::{char, i8},
     combinator::map_parser,
+    error::{Error, ErrorKind},
     sequence::terminated,
 };
 
@@ -30,25 +35,43 @@ fn parse_double_digit(input: &str) -> IResult<&str, i8> {
     map_parser(take(2usize), i8).parse(input)
 }
 
-fn get_date_form_fields(cmp: &Zoned, day: i8, hour: i8, minute: i8) -> Zoned {
-    let ordering = day
-        .cmp(&cmp.day())
-        .then_with(|| hour.cmp(&cmp.hour()))
-        .then_with(|| minute.cmp(&cmp.minute()));
-
-    let mut year = cmp.year();
-    let month = if ordering == std::cmp::Ordering::Less {
-        cmp.month()
-    } else if cmp.month() == 1 {
-        year -= 1;
-        12
+fn previous_month(year: i16, month: i8) -> (i16, i8) {
+    if month == 1 {
+        (year - 1, 12)
     } else {
-        cmp.month() - 1
-    };
-    date(year, month, day)
-        .at(hour, minute, 0, 0)
+        (year, month - 1)
+    }
+}
+
+fn build_candidate(
+    cmp: &Zoned,
+    year: i16,
+    month: i8,
+    day: i8,
+    hour: i8,
+    minute: i8,
+) -> Option<Zoned> {
+    let date = Date::new(year, month, day).ok()?;
+    let time = Time::new(hour, minute, 0, 0).ok()?;
+    DateTime::from_parts(date, time)
         .to_zoned(cmp.time_zone().clone())
-        .unwrap()
+        .ok()
+}
+
+fn get_date_form_fields(cmp: &Zoned, day: i8, hour: i8, minute: i8) -> Option<Zoned> {
+    let mut year = cmp.year();
+    let mut month = cmp.month();
+
+    for _ in 0..12 {
+        if let Some(candidate) = build_candidate(cmp, year, month, day, hour, minute)
+            && candidate.timestamp() <= cmp.timestamp()
+        {
+            return Some(candidate);
+        }
+        (year, month) = previous_month(year, month);
+    }
+
+    None
 }
 
 pub(crate) fn nom_metar_timestamp(input: &str) -> IResult<&str, Timestamp> {
@@ -68,13 +91,15 @@ fn nom_metar_timestamp_with_zone<'a>(
     .parse(input)?;
     let (day, hour, minute) = fields;
     *refernce_time += jiff::SignedDuration::from_hours(1);
-    let timestamp = get_date_form_fields(refernce_time, day, hour, minute);
+    let timestamp = get_date_form_fields(refernce_time, day, hour, minute)
+        .ok_or_else(|| nom::Err::Error(Error::new(input, ErrorKind::Verify)))?;
     Ok((rest, Timestamp { timestamp }))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use jiff::civil::date;
 
     fn make_test_timestamp() -> Zoned {
         date(2025, 06, 28)
@@ -127,6 +152,26 @@ mod tests {
                 .to_zoned(day.time_zone().clone())
                 .unwrap(),
         );
+        assert_eq!(
+            nom_metar_timestamp_with_zone(input, &mut day),
+            Ok(("", expected))
+        );
+    }
+
+    #[test]
+    fn test_metar_skips_invalid_previous_month() {
+        let input = "291250Z";
+        let mut day = date(2026, 03, 22)
+            .at(16, 0, 0, 0)
+            .to_zoned(TimeZone::UTC)
+            .unwrap();
+        let expected = Timestamp::new(
+            date(2026, 01, 29)
+                .at(12, 50, 0, 0)
+                .to_zoned(day.time_zone().clone())
+                .unwrap(),
+        );
+
         assert_eq!(
             nom_metar_timestamp_with_zone(input, &mut day),
             Ok(("", expected))
